97d0ae0d8fc4a587c98440192cc0ab6e38c9a1d5
Zhengyang Liu <liuz@cs.utah.edu>
2017-09-24 22:02:36 -0600

    Disabled instcombine

--- lib/CodeGen/BackendUtil.cpp
+++ lib/CodeGen/BackendUtil.cpp
@@ -251,7 +251,7 @@ static void addMemorySanitizerPass(const PassManagerBuilder &Builder,
     PM.add(createReassociatePass());
     PM.add(createLICMPass());
     PM.add(createGVNPass());
-    PM.add(createInstructionCombiningPass());
+    //    PM.add(createInstructionCombiningPass());
     PM.add(createDeadStoreEliminationPass());
   }
 }
